87a3f4842cf318534a21936ee1904485ccc25b23
Andrey Andriychuk <andrey@halalbooking.com>
2020-01-18 20:11:13 +0300

    Kinda server

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject clojure-simple-http "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :author "Andrey Andriychuk"
+  :description "A simple HTTP server"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
-  :repl-options {:init-ns clojure-simple-http.core})
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [http-kit "2.3.0"]
+                 [clj-time "0.15.2"]]
+  :repl-options {:init-ns clojure-simple-http.core}
+  :main clojure-simple-http.core)
--- src/clojure_simple_http/core.clj
+++ src/clojure_simple_http/core.clj
@@ -1,6 +1,12 @@
-(ns clojure-simple-http.core)
+(ns clojure-simple-http.core
+  (:require [org.httpkit.server :refer [run-server]]
+            [clj-time.core :as t]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn app [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    (str (t/time-now))})
+
+(defn -main [& args]
+  (run-server app {:port 8080})
+  (println "Server started on port 8080"))
